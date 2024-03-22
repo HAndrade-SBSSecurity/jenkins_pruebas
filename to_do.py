b443be28abaa6e9852ab88acd91dcdbf6e04e5fc
HAndrade-SBSSecurity <hans.andrade@sbssecurity.io>
2024-03-22 14:38:56 -0400

    Actualización lista

--- to_do.py
+++ to_do.py
@@ -30,7 +30,6 @@ def copy_repo_contents(source_url, dest_url):
 copy_repo_contents(repo_do_url, repo_to_do_url)"""
 
 import sys
-import time
 
 def export_list(lista):
     # Puedes realizar cualquier operación que necesites con la lista aquí
@@ -44,6 +43,5 @@ if __name__ == "__main__":
     lista = sys.argv[1:]
 
     # Llamar a la función para exportar la lista
-    while(True):
-        time.sleep(21)
-        export_list(lista)
+    
+    export_list(lista)
