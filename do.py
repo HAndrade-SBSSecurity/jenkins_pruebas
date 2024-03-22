97d40b8508238b49c9ecb9a01b54bf280eacbac7
HAndrade-SBSSecurity <hans.andrade@sbssecurity.io>
2024-03-22 12:04:06 -0400

    Actualización lista

--- do.py
+++ do.py
@@ -9,11 +9,11 @@ def update_list(lista):
 
 # Clonar el repositorio "do"
 subprocess.run(["git", "clone", "https://github.com/HAndrade-SBSSecurity/do.git"])
+subprocess.run(["git", "init", "do/"])  # Inicializar el directorio 'do/' como un repositorio Git
 lista = []  # Inicializar lista vacía
-
 while True:
     lista = update_list(lista)  # Actualizar la lista
-    subprocess.run(["git", "add", "do.py"])  # Agregar todos los cambios    
+    subprocess.run(["git", "add", "do.py"])  # Agregar el archivo do.py
     subprocess.run(["git", "add", "do/"])  # Agregar el directorio do/
     subprocess.run(["git", "commit", "-m", "Actualización lista"])  # Hacer commit
     subprocess.run(["git", "pull"])  # Realizar un pull en el repositorio
