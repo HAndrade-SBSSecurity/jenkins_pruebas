8be8e46353d4cc353a42d4d69ca2fecb68be33d1
HAndrade-SBSSecurity <hans.andrade@sbssecurity.io>
2024-03-22 11:11:02 -0400

    start

---
+++ do.py
@@ -0,0 +1,20 @@
+import subprocess
+import time
+
+# Función para actualizar la lista
+def update_list(lista):
+    lista.append(len(lista))  # Agregar un elemento a la lista
+    return lista
+
+# Clonar el repositorio "do"
+subprocess.run(["git", "clone", "https://github.com/HAndrade-SBSSecurity/do.git"])
+lista = []  # Inicializar lista vacía
+
+while True:
+    lista = update_list(lista)  # Actualizar la lista
+    time.sleep(60)  # Esperar 1 minuto
+    if len(lista) % 3 == 0:  # Cada 3 minutos
+        subprocess.run(["git", "pull"])  # Realizar un pull en el repositorio
+        subprocess.run(["git", "add", "."])  # Agregar todos los cambios
+        subprocess.run(["git", "commit", "-m", "Actualización automática"])  # Hacer commit
+        subprocess.run(["git", "push"])  # Realizar un push
---
+++ to_do.py
@@ -0,0 +1,30 @@
+import requests
+
+# Variables con los datos de los repositorios
+repo_do_url = "https://api.github.com/repos/HAndrade-SBSSecurity/do/contents"
+repo_to_do_url = "https://api.github.com/repos/HAndrade-SBSSecurity/to_do/contents"
+headers = {"HAndrade-SBSSecurity": "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"}
+
+# Función para clonar el repositorio "do" y copiar el contenido al repositorio "to do"
+def copy_repo_contents(source_url, dest_url):
+    response = requests.get(source_url, headers=headers)
+    files = response.json()
+
+    for file in files:
+        if file["type"] == "file":
+            response_file = requests.get(file["download_url"], headers=headers)
+            content = response_file.json()["content"]
+
+            data = {
+                "message": "Copiar archivo " + file["name"],
+                "content": content,
+                "branch": "main"
+            }
+
+            requests.put(dest_url + "/" + file["name"], headers=headers, json=data)
+
+        elif file["type"] == "dir":
+            copy_repo_contents(file["url"], dest_url + "/" + file["name"])
+
+# Clonar el repositorio "do" y copiar el contenido al repositorio "to do"
+copy_repo_contents(repo_do_url, repo_to_do_url)
