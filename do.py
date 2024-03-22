f244a4b9af45fabe25173897f0bf9b131c815283
HAndrade-SBSSecurity <hans.andrade@sbssecurity.io>
2024-03-22 11:51:45 -0400

    Actualización lista

--- do.py
+++ do.py
@@ -4,6 +4,7 @@ import time
 # Función para actualizar la lista
 def update_list(lista):
     lista.append(len(lista))  # Agregar un elemento a la lista
+    print(lista)
     return lista
 
 # Clonar el repositorio "do"
@@ -12,9 +13,13 @@ lista = []  # Inicializar lista vacía
 
 while True:
     lista = update_list(lista)  # Actualizar la lista
-    time.sleep(60)  # Esperar 1 minuto
-    if len(lista) % 3 == 0:  # Cada 3 minutos
+    subprocess.run(["git", "add", "do.py"])  # Agregar todos los cambios    
+    subprocess.run(["git", "commit", "-m", "Actualización lista"])  # Hacer commit
+    subprocess.run(["git", "pull"])  # Realizar un pull en el repositorio
+
+    time.sleep(30)  # Esperar 1 minuto
+    """if len(lista) % 3 == 0:  # Cada 3 minutos
         subprocess.run(["git", "pull"])  # Realizar un pull en el repositorio
         subprocess.run(["git", "add", "."])  # Agregar todos los cambios
         subprocess.run(["git", "commit", "-m", "Actualización automática"])  # Hacer commit
-        subprocess.run(["git", "push"])  # Realizar un push
+        subprocess.run(["git", "push"])  # Realizar un push"""
